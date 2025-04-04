ece823817150ace9b6569d413aea0d94c6faa293
Lucas Grangeiro <lucaslg6401@gmail.com>
2025-04-04 20:05:52 -0300

    :sparkles: feat: adicionado Post request

--- src/main/java/com/lucas/crud_spring/controller/CourseController.java
+++ src/main/java/com/lucas/crud_spring/controller/CourseController.java
@@ -2,10 +2,9 @@ package com.lucas.crud_spring.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.lucas.crud_spring.model.Course;
 import com.lucas.crud_spring.repository.CourseRepository;
@@ -22,5 +21,11 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
